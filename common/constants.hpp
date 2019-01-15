6755e764b84f58ec71a7bd1f783c9b4aad44f675
Shane <shane@yztz.cn>
2019-01-15 12:04:36 +0800

    switch testnet to also use houseaccount as house

--- common/constants.hpp
+++ common/constants.hpp
@@ -1,12 +1,5 @@
 #pragma once
 
-#define MAIN_NET
-
 #define EOS_TOKEN_CONTRACT name("eosio.token")
 #define EOS_SYMBOL symbol("EOS", 4)
-
-#ifdef MAIN_NET
-    #define HOUSE_ACCOUNT name("houseaccount")
-#else
-    #define HOUSE_ACCOUNT name("godapphouse1")
-#endif
+#define HOUSE_ACCOUNT name("houseaccount")
