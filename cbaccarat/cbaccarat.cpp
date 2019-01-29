de3b0955e1a527c9827282660aa2fbe44ba020d1
Shane <shane@yztz.cn>
2019-01-29 14:27:08 +0800

    fix baccarat contract

--- cbaccarat/cbaccarat.cpp
+++ cbaccarat/cbaccarat.cpp
@@ -6,75 +6,78 @@
 
 #define BET_BANKER_WIN              1
 #define BET_PLAYER_WIN              2
-#define BET_TIE                     3
-#define BET_BANKER_PAIR             4
-#define BET_PLAYER_PAIR             5
+#define BET_TIE                     4
+#define BET_BANKER_PAIR             8
+#define BET_PLAYER_PAIR             16
 
 namespace godapp {
     DEFINE_STANDARD_FUNCTIONS(cbaccarat)
-
-    string result_to_string(uint8_t result) {
-        switch (result & 7) {
-            case BET_BANKER_WIN:
-                return "Banker Wins";
-            case BET_PLAYER_WIN:
-                return "Player Wins";
-            case BET_TIE:
-                return "Tie";
-            default:
-                return "";
-        }
-    }
 
     class cbaccarat_result {
     public:
         vector<card_t> banker_cards, player_cards;
         bool banker_pair, player_pair;
         uint8_t banker_point, player_point;
+        uint8_t game_result;
         uint8_t result;
 
         cbaccarat_result() {
             draw_cards(banker_cards, banker_point, player_cards, player_point);
 
             if (player_point > banker_point) {
-                result = BET_BANKER_WIN;
+                game_result = BET_PLAYER_WIN;
             } else if (banker_point > player_point) {
-                result = BET_BANKER_WIN;
+                game_result = BET_BANKER_WIN;
             } else {
-                result = BET_TIE;
+                game_result = BET_TIE;
             }
 
             banker_pair = card_value(banker_cards[0]) == card_value(banker_cards[1]);
             player_pair = card_value(player_cards[0]) == card_value(player_cards[1]);
+
+            result = game_result | (banker_pair ? BET_BANKER_PAIR : 0) | (player_pair ? BET_PLAYER_PAIR : 0);
         }
 
         asset get_payout(const cbaccarat::bet& bet_item) {
             asset bet = bet_item.bet;
             switch (bet_item.bet_type) {
                 case BET_BANKER_WIN:
-                    if (result == BET_BANKER_WIN) {
+                    if (game_result == BET_BANKER_WIN) {
                         return bet * 195 / 100;
-                    } else if (result == BET_TIE) {
+                    } else if (game_result == BET_TIE) {
                         return bet;
                     } else {
                         return asset(0, bet.symbol);
                     }
                 case BET_PLAYER_WIN:
-                    if (result == BET_PLAYER_WIN) {
+                    if (game_result == BET_PLAYER_WIN) {
                         return bet * 2;
-                    } else if (result == BET_TIE) {
+                    } else if (game_result == BET_TIE) {
                         return bet;
                     } else {
                         return asset(0, bet.symbol);
                     }
                 case BET_TIE:
-                    return result == BET_TIE ? (bet * 9) : asset(0, bet.symbol);
+                    return game_result == BET_TIE ? (bet * 9) : asset(0, bet.symbol);
                 case BET_BANKER_PAIR:
                     return banker_pair ? (bet * 12) : asset(0, bet.symbol);
                 case BET_PLAYER_PAIR:
                     return player_pair ? (bet * 12) : asset(0, bet.symbol);
                 default:
                     return asset(0, bet.symbol);
+            }
+        }
+
+        string result_string() {
+            switch (game_result) {
+                case BET_BANKER_WIN:
+                    return "Banker Wins";
+                case BET_PLAYER_WIN:
+                    return "Player Wins";
+                case BET_TIE:
+                    return "Tie";
+                default:
+                    return "";
             }
         }
 
@@ -86,7 +89,7 @@ namespace godapp {
         void set_receipt(cbaccarat& contract, uint64_t game_id) {
             SEND_INLINE_ACTION(contract, receipt, {contract.get_self(), name("active")},
                                {game_id, cards_to_string(player_cards), player_point,
-                                   cards_to_string(banker_cards), banker_point, result_to_string(result)});
+                                   cards_to_string(banker_cards), banker_point, result_string()});
         }
     };
 
