b85734fc76d12cc7e0f96150f662612f073cbe75
Shane <shane@yztz.cn>
2019-04-17 16:40:15 +0800

    账户及任务系统

--- house/house.hpp
+++ house/house.hpp
@@ -62,6 +62,31 @@ namespace godapp {
                 indexed_by< name("byeventin"), const_mem_fun<player, uint64_t, &player::byeventin> >
         > player_index;
 
+        TABLE player_record {
+            name player;
+            uint64_t in;
+            uint64_t out;
+            uint64_t play_times;
+            uint32_t last_play_time;
+
+            name referer;
+            uint64_t referer_payout;
+
+            uint64_t game_played_flag;
+            uint64_t bonus_claimed;
+            uint64_t bonus_point;
+
+            uint64_t daily_in;
+            uint64_t daily_flag;
+            uint64_t daily_claimed;
+
+            uint64_t primary_key() const {return player.value;};
+            uint64_t byreferer() const {return referer.value;}
+        };
+        typedef multi_index<name("playerrecord"), player_record,
+            indexed_by< name("byreferer"), const_mem_fun<player_record, uint64_t, &player_record::byreferer> >
+        > player_record_index;
+
         house(name receiver, name code, datastream<const char *> ds): contract(receiver, code, ds) {
         }
 
