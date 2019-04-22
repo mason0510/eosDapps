07a4e73a5f0fecced89b8991130252175a1134e9
Shane <shane@yztz.cn>
2019-04-22 18:45:08 +0800

    house 记录的调整

--- house/house.cpp
+++ house/house.cpp
@@ -279,11 +279,10 @@ namespace godapp {
 #define SET_REFERER             0
 #define PLAY_ANY_GAME           1
 #define PLAY_ALL_GAMES          2
-#define TOTAL_PLAYED_START      30
-#define TOTAL_PLAYED_END        37
+#define TOTAL_PLAYED_START      20
+#define TOTAL_PLAYED_END        27
 
-#define CHEST_OPEN_START        50
-#define CHEST_OPEN_END          54
+#define CHEST_OPEN_END          4
 
     struct played_reward {
         uint64_t amount;
@@ -297,7 +296,7 @@ namespace godapp {
 
     void house::claimreward(name player, uint8_t reward_type) {
         require_auth(player);
-        uint64_t reward_mask = 1 << reward_type;
+        uint64_t reward_mask = ((uint32_t) 1) << reward_type;
 
         player_record_index game_player(_self, _self.value);
         auto itr = game_player.find(player.value);
@@ -348,9 +347,8 @@ namespace godapp {
     void house::openchest(name player, uint8_t chest_type) {
         require_auth(player);
 
-        uint8_t reward_type = CHEST_OPEN_START + chest_type;
-        uint64_t reward_mask = 1 << reward_type;
-        eosio_assert(reward_type <= CHEST_OPEN_END, "Invalid Chest type");
+        uint64_t reward_mask = ((uint32_t) 1) << chest_type;
+        eosio_assert(chest_type <= CHEST_OPEN_END, "Invalid Chest type");
         chest_reward reward_value = chest_amount[chest_type];
         asset reward(reward_value.amount, EOS_SYMBOL);
 
