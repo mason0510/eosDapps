ac4c0533f9eab63d54c7876dc411ca060f143da4
Shane <shane@yztz.cn>
2019-04-25 14:19:19 +0800

    修正中心化合约

--- centergame/centergame.cpp
+++ centergame/centergame.cpp
@@ -11,62 +11,58 @@
 #include "../common/tables.hpp"
 #include "../common/param_reader.hpp"
 #include "../common/game_contracts.hpp"
-#define BET_START 1
-#define MAX_BET_AMOUNT 60000
-#define MIN_BETAMOUNT 1000
-#define EOS_SYMBOL symbol("EOS", 4)
+
 namespace godapp {
     centergame::centergame(name receiver, name code, datastream<const char *> ds) :
         contract(receiver, code, ds),
-        _bets(_self, _self.value),
-        _betprizes(_self, _self.value) {
-        }
-
-
-   void centergame::transfer(name from, name to, asset quantity, string memo) {
-        if (!check_transfer(this, from, to, quantity, memo)) {
-            return;
-        };
-
-       transfer_to_house(_self, quantity, from, quantity.amount); 
-        print(from.to_string() );
-        print(to.to_string());
-        print("memo",memo.c_str() );
-
-        param_reader reader(memo); 
-        auto game_id = (uint64_t) atol( reader.next_param("Game ID cannot be empty!").c_str() ); 
-        auto referer = reader.get_referer(from); 
-        uint32_t timestamp = now(); 
-        asset total = asset(0, quantity.symbol); 
-        while(reader.has_next()) { 
-            string self_content  = reader.next_param().c_str(); 
-            uint64_t next_bet_id = _bets.available_primary_key();
-            _bets.emplace(_self, [&](auto &a) { 
-                a.id = next_bet_id; 
-                a.game_id = game_id; 
-                a.bet_asset=quantity;
-                a.player = from; 
-                a.referer = referer;
-                a.self_content = self_content; 
-                a.time = timestamp; 
-            }); 
-        } 
+        _bets(_self, _self.value) {
     }
 
-void centergame::reveal(uint8_t game_id,std::vector<uint8_t>& betIds,std::vector<name>& reward_names,std::vector<asset>& prize_amounts) {
-   require_auth(_self);
-   for (int i=0; i<betIds.size(); i++) {
-    asset prize_amount= prize_amounts[i];
-    uint8_t betid= betIds[i];
-    name player=reward_names[i];
-     require_recipient( player );
-    auto itr = _bets.find(betid);
-    action(
-            permission_level{_self,name("active")},
-            name("houseaccount"),                     
-            name("pay"),                         
-            std::make_tuple(game_id, player,itr->bet_asset,prize_amount,"Dapp365 Game win!",itr->referer)    
-            ).send(); 
-}
-}
+
+    void centergame::transfer(name from, name to, asset quantity, string memo) {
+        if (!check_transfer(this, from, to, quantity, memo)) {
+            return;
+        }
+
+        transfer_to_house(_self, quantity, from, quantity.amount);
+        param_reader reader(memo); 
+        auto game_id = (uint64_t) atol( reader.next_param("Game ID cannot be empty!").c_str() );
+        auto referer = reader.get_referer(from);
+        auto message = reader.rest();
+
+        uint32_t timestamp = now();
+        uint64_t next_bet_id = _bets.available_primary_key();
+        _bets.emplace(_self, [&](auto &a) {
+            a.id = next_bet_id;
+            a.game_id = game_id;
+            a.bet_asset = quantity;
+            a.player = from;
+            a.referer = referer;
+            a.self_content = message;
+            a.time = timestamp;
+        });
+    }
+
+    void centergame::reveal(uint8_t game_id, const std::string& message, const std::vector<uint8_t>& bet_ids,
+        const std::vector<asset>& prize_amounts) {
+        require_auth(_self);
+        for (int i=0; i<bet_ids.size(); i++) {
+            asset prize_amount = prize_amounts[i];
+            uint8_t betId = bet_ids[i];
+            auto itr = _bets.find(betId);
+
+            name player = itr->player;
+            delayed_action(_self, player, name("payment"),
+                make_tuple(game_id, player, itr->referer, message, itr->bet_asset, prize_amount), 0);
+
+            _bets.erase(itr);
+        }
+    }
+
+    void centergame::payment(uint64_t id, name player, name referer, const std::string& message, asset bet, asset payout) {
+        require_auth(_self);
+        require_recipient(player);
+
+        make_payment(_self, player, bet, payout, referer, message);
+    }
 }
--- centergame/centergame.hpp
+++ centergame/centergame.hpp
@@ -14,11 +14,8 @@ namespace godapp {
     CONTRACT centergame: public contract {
     public:
         using contract::contract;
-        std::vector<uint8_t> prize_amounts;
-        std::vector<uint8_t> betIds;
-        std::vector<name> reward_names;
 
-      TABLE player_record {
+        TABLE bet_record {
             uint64_t id;
             name player;
             uint64_t game_id;
@@ -28,19 +25,15 @@ namespace godapp {
             uint64_t time; 
             uint64_t primary_key() const { return id; };
         };
-        typedef multi_index<name("playerrecord"), player_record> bet_record_index; bet_record_index _bets;
-       
-         TABLE betsprize_records {
-            name player;
-            uint64_t game_id;
-            uint64_t prize_amount;
-            uint32_t prize_time;
-            uint64_t primary_key() const { return player.value; };
-        };
-        typedef eosio::multi_index<"prizerecords"_n, betsprize_records> prize_index;prize_index _betprizes;
-    ACTION transfer(name from, name to, asset quantity, string memo);
-    ACTION reveal(uint8_t game_id,std::vector<uint8_t>& betIds,std::vector<name>& reward_names,std::vector<asset>& prize_amounts);
-    centergame(name receiver, name code, datastream<const char *> ds);
+        typedef multi_index<name("bets"), bet_record> bet_record_index;
+        bet_record_index _bets;
+
+        ACTION transfer(name from, name to, asset quantity, string memo);
+        ACTION reveal(uint8_t game_id, const std::string& message, const std::vector<uint8_t>& bet_ids,
+            const std::vector<asset>& prize_amounts);
+        ACTION payment(uint64_t id, name player, name referer, const std::string& message, asset bet, asset payout);
+
+        centergame(name receiver, name code, datastream<const char *> ds);
    };
   EOSIO_ABI_EX(centergame, (transfer)(reveal))
 }
